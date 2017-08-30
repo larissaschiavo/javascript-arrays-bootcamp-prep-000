568a70e8943c38851e7fc17ce8012743bc37c5cf
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-30 22:12:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,9 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
   var n = [element, ...array]
-  return n
+  return n;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-    const arr = array
     array.unshift(element);
 }
